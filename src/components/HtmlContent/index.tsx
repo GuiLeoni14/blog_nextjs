67041cf5bb99267cc3ad0ed6b90ad5adaaa0782f
GuiLeoni14 <guileone167@gmail.com>
2022-08-07 20:13:14 -0300

    ajustando code highlight

--- src/components/HtmlContent/index.tsx
+++ src/components/HtmlContent/index.tsx
@@ -13,6 +13,8 @@ export const HtmlContent = ({ html }: HtmlContentProps) => {
         // let removeAds = null;
         setComponentIsMounted(true);
         if (typeof window !== 'undefined' && componentIsMounted) {
+            const all_tag_code = document.querySelectorAll('.html_content code');
+            all_tag_code.forEach((tag) => (tag.className = 'language-javascript'));
             Prism.highlightAll();
             // removeAds = setTimeout(() => {
             //     document.querySelectorAll('iframe[src*=ads]').forEach((el) => el.remove());
@@ -23,7 +25,9 @@ export const HtmlContent = ({ html }: HtmlContentProps) => {
 
     return (
         <PrismWrapper>
-            {componentIsMounted && <Styled.Container dangerouslySetInnerHTML={{ __html: html }} />}
+            {componentIsMounted && (
+                <Styled.Container dangerouslySetInnerHTML={{ __html: html }} className="html_content" />
+            )}
         </PrismWrapper>
     );
 };
--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -3,7 +3,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 
 export const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
-    return format(date, "EEEE' • 'd' de 'MMMM'", {
+    return format(date, "dd' / 'MM' / 'yy'", {
         locale: ptBR,
     });
 };
